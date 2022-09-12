4c38c94b77979d9c8e2add6c800e08fe663419c7
Subaru Sakaguchi <subarusakaguchi@yahoo.com.br>
2022-09-12 00:28:54 -0300

    Mobile configured

---
+++ mobile/App.tsx
@@ -0,0 +1,35 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+      <StatusBar style="auto" />
+      <Button title='ENVIAR' />
+    </View>
+  );
+}
+
+interface ButtonProps {
+  title: string
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
